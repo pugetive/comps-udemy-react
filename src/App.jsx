f967f10be843a122bc79e622c870d2ad06e0a4d2
Todd Gehman <github@pugetive.com>
2023-03-20 12:34:38 -0400

    finished lesson 156: placeholder button comp

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import Button from "./Button";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Button>Click me!</Button>
+      </div>
+      <div>
+        <Button>Buy now</Button>
+      </div>
+      <div>
+        <Button>See deal!</Button>
+      </div>
+      <div>
+        <Button>Hide ads</Button>
+      </div>
+      <div>
+        <Button>Something!</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
